cdb1af9e0b86ab39f980d1bd0188b57a6ac1d0d6
Alexander Burtsev <eburus@gmail.com>
2013-07-16 23:40:23 +0400

    add consumer labels form

--- src/spicy/labels/forms.py
+++ src/spicy/labels/forms.py
@@ -1,7 +1,37 @@
-from . import models
 from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+from . import models
+
 
 class LabelForm(forms.ModelForm):
     class Meta:
         model = models.Label
         fields = 'text', 'order_lv'
+
+
+class LabelConsumerForm(forms.ModelForm):
+    labels = forms.CharField(label=_('Labels'), required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(LabelConsumerForm, self).__init__(*args, **kwargs)
+
+        self.fields['labels'].initial = ', '.join([lb.text for lb in self.instance.label_set.all()])
+
+    def save(self, *args, **kwargs):
+        instance = super(LabelConsumerform, self).save(*args, **kwargs)
+
+        labels = self.cleaned_data['labels'].split(',')
+
+        if labels:
+            exists_labels = list(models.Label.objects.filter(text__in=labels))
+
+            #create new labels
+            for label in (set(labels) - set([lb.text for lb in exists_labels])):
+                label, created = models.Label.objects.get_or_create(text=label)
+                exists_labels.append(label)
+
+            instance.label_set.clear()
+            instance.label_set.add(*exists_labels)
+
+        return instance
