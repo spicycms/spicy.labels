0adae9648662bbaa1473d048e9fbbc6f132a8020
Stas Shtin <antisvin@gmail.com>
2013-08-28 22:34:12 +0400

    Broken shit

--- src/spicy/labels/templatetags/labels.py
+++ src/spicy/labels/templatetags/labels.py
@@ -1,4 +1,3 @@
-from blog import defaults as bl_defaults
 from django import template
 from django.db.models import Count
 from spicy.labels import models
@@ -11,10 +10,8 @@ register = template.Library()
 def top_labels(num):
     now = datetime.datetime.now()
     return models.Label.objects.filter(
-        document__is_public=True, document__pub_date__lte=now,
-        document__state=bl_defaults.STATE_APPROVED
-        ).values('text').annotate(
-        Count('text')).order_by('-text__count')[:num]
+            document__is_public=True, document__pub_date__lte=now,
+        ).values('text').annotate(Count('text')).order_by('-text__count')[:num]
 
 
 @register.simple_tag(takes_context=True)
