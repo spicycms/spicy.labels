c079dad777b34d6591b2ec0542a0d4e984c65658
Alexandr Zolotarev <megabogus84@gmail.com>
2013-09-24 22:32:30 +0400

    #1124 вывод нужных доков

--- src/spicy/labels/views.py
+++ src/spicy/labels/views.py
@@ -2,13 +2,20 @@
 from spicy.core.siteskin.decorators import render_to
 from spicy.labels import models
 from django.shortcuts import get_object_or_404
+from spicy.presscenter import defaults
+from spicy.utils.models import get_custom_model_class
 
+Document = get_custom_model_class(defaults.CUSTOM_DOCUMENT_MODEL)
 
 @render_to('spicy.labels/label.html', use_siteskin=True)
 def label(request, slug):
     slug = slug.split('+')
-    docs = []
+    labels = []
     for s in slug:
         obj = get_object_or_404(models.Label, slug=s)
-        docs.append(obj)
-    return {'docs': docs}
+        labels.append(obj)
+    try:
+        docs = Document.pub_objects.filter(label__in=labels)
+    except AttributeError:
+        docs = Document.objects.filter(label__in=labels)
+    return {'docs': docs, 'labels': labels}
